f6fc4fbc2fff75a5c22119761977717aa000a58b
stormi <stormikhashimoto@gmail.com>
2020-08-07 21:33:57 -0700

    Up to steps complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const obj = {};
+	let max = 0;
+	let maxChar = '';
+
+	for (let char of str) {
+		if (obj[char]) {
+			obj[char]++;
+		} else {
+			obj[char] = 1;
+		}
+	}
+	for (let char in obj) {
+		if (obj[char] > max) {
+			max = obj[char];
+			maxChar = char;
+		}
+	}
+	return maxChar;
+}
 
 module.exports = maxChar;
